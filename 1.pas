e320a22780a9d9b75a57378fd49756eb1b62ccc0
fegyiwnl <60046344+fegyiwnl@users.noreply.github.com>
2022-11-11 16:01:14 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,45 @@
+﻿var
+  
+  a: array[1..50] of integer; 
+  n, i, s, ss, b: integer; 
+  
+begin
+  
+  write('Введите количество элементов массива: ');
+  Readln(n);
+  
+  for i:= 1 to n do     
+   begin
+     write('Введите значение элемента ',i,': ');
+     readln(a[i]);
+   end;
+  writeln('Исходный массив:');
+   
+  for i:= 1 to n do      
+    begin
+      write(a[i]:4);
+      if i mod 10 = 0 then
+        writeln;
+    end;
+    
+  writeln;
+  write('Введите значение числа s: ');
+  readln(s);
+  ss := 0;
+  
+  for i:= 1 to n do
+    begin
+      ss := ss + a[i];  
+      if ss > s then    
+        begin
+          b := i;
+          break;
+        end;
+    end;
+    
+  if ss < n then
+    writeln('Сумма элементов массива не достигла значение числа s')
+  else
+    writeln('Количество элементов массива, сумма которых превысила число s: ', b);
+
+end.
---
+++ 2.pas
@@ -0,0 +1,56 @@
+﻿var
+  
+  i, j, k, m: integer;
+  s, t: string;
+  a: array[1..50] of string;
+  n: array[1..50] of integer;
+
+procedure aa;
+
+var
+  
+  k: integer; 
+  
+begin
+  
+  if t <> '' then
+    for k := 1 to j do 
+      if a[k] = t then 
+      begin
+        n[k] := n[k] + 1;
+        if n[m] < n[k] then 
+          m := k;
+        t := '';
+        break;
+      end;
+      
+  if t <> '' then begin
+    j := j + 1;
+    n[j] := 1;
+    a[j] := t;
+    t := ''
+  end;
+  
+end;
+ 
+begin
+  
+  i := 0;
+  j := 0;
+  m := 1;
+  t := '';
+  write('Введите слова: ');
+  readln(s);
+  
+  for i := 1 to length(s) do 
+    if s[i] <> ' ' then 
+      t := t + s[i] 
+      else aa;
+      
+  for i := 1 to j do 
+  begin
+    if n[i] = n[m] then 
+      writeln(a[i]);
+  end;
+  
+end.
