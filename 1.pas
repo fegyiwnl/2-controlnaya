b3fa89eb7fd1f38675373d559d3267dcd89e75cd
fegyiwnl <60046344+fegyiwnl@users.noreply.github.com>
2022-11-15 12:31:41 +0300

    Update 1.pas

--- 1.pas
+++ 1.pas
@@ -1,45 +1,44 @@
-﻿var
-  
-  a: array[1..50] of integer; 
-  n, i, s, ss, b: integer; 
-  
+
+
 begin
+  var a: array[1..50] of integer; 
+  var s: integer;
+  var ss := 0;
+  var b: integer;
+  var n := ReadInteger('Введите размер массива: ');
   
-  write('Введите количество элементов массива: ');
-  Readln(n);
+  for var i := 1 to n do     
+  begin
+    print('Введите значение элемента ', i, ': ');
+    a[i] := ReadInteger();
+  end;
+  print('Исходный массив:');
   
-  for i:= 1 to n do     
-   begin
-     write('Введите значение элемента ',i,': ');
-     readln(a[i]);
-   end;
-  writeln('Исходный массив:');
-   
-  for i:= 1 to n do      
-    begin
-      write(a[i]:4);
-      if i mod 10 = 0 then
-        writeln;
-    end;
-    
+  for var i := 1 to n do      
+  begin
+    write(a[i]:4);
+    if i mod 10 = 0 then
+      writeln;
+  end;
+  
   writeln;
-  write('Введите значение числа s: ');
-  readln(s);
+  print('Введите значение числа s: ');
+  s := ReadInteger();
   ss := 0;
   
-  for i:= 1 to n do
+  for var i := 1 to n do
+  begin
+    ss += a[i];  
+    if ss > s then    
     begin
-      ss := ss + a[i];  
-      if ss > s then    
-        begin
-          b := i;
-          break;
-        end;
+      b := i;
+      break;
     end;
-    
+  end;
+  
   if ss < n then
-    writeln('Сумма элементов массива не достигла значение числа s')
+    print('Сумма элементов массива не достигла значение числа s')
   else
-    writeln('Количество элементов массива, сумма которых превысила число s: ', b);
-
+    print('Количество элементов массива, сумма которых превысила число s: ', b);
+  
 end.
